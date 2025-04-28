31181f277eb3e8283c519f4cc4deee565c7db344
davi1506 <davi1506@purdue.edu>
2025-04-28 12:56:01 -0400

    Working copy without encryption. Will focus on optimizing audio quality.

--- src/main.cpp
+++ src/main.cpp
@@ -27,10 +27,12 @@
 #define I2S_PORT_OUT I2S_NUM_1
 
 #define AUDIO_SAMPLE_RATE     48000//8000  // 44100 init 8000
-#define AUDIO_OPUS_FRAME_MS   5   // one of 2.5, 5, 10, 20, 40, 60, 80, 100, 120 init 40
-#define AUDIO_OPUS_BITRATE    64000// bit rate from 2400 to 512000
+#define AUDIO_OPUS_FRAME_MS   20   // one of 2.5, 5, 10, 20, 40, 60, 80, 100, 120 init 40
+#define AUDIO_OPUS_BITRATE    16000// bit rate from 2400 to 512000
 #define AUDIO_OPUS_COMPLEXITY 0     // from 0 to 10
 #define OPUS_FRAME_SIZE ((AUDIO_SAMPLE_RATE / 1000) * AUDIO_OPUS_FRAME_MS)
+
+#define IDENTIFIER 2120
 
 #define TASK_STACK_SIZE 16384
 
@@ -46,28 +48,32 @@
 #define LoRa_NRST 12
 #define LoRa_BUSY 13
 
-#define LoRa_BANDWIDTH 800.0F
+#define LoRa_BANDWIDTH 500.0F
 #define LoRa_CODING_RATE_DENOM 5
 #define LoRa_FREQUENCY 920.0F
-#define LoRa_POWER 22
+#define LoRa_POWER 17
 #define LoRa_PREAMBLE_LEN 10U
 #define LoRa_SPREADFACTOR 5
-#define LoRa_SYNCWORD 0x11
+#define LoRa_SYNCWORD 0xF5
 #define LoRa_TCXO_VOLTAGE 1.6
 #define LoRa_USE_REGULATOR_LDO false
 
+#define GAIN_FACTOR 2
+
 //SX1262 radio = new Module(LoRa_NSS, LoRa_DIO1, LoRa_NRST, LoRa_BUSY);
 
-//Module *lora_module = new Module(LoRa_NSS, LoRa_DIO1, LoRa_NRST, LoRa_BUSY);
+Module *lora_module; //= new Module(LoRa_NSS, LoRa_DIO1, LoRa_NRST, LoRa_BUSY);
 
 // Then pass it to SX1262
-//SX1262 radio(lora_module);
+SX1262 *radio; //(lora_module);
 
 OpusEncoder *opus_encoder_;
 OpusDecoder *opus_decoder_;
 
 void onSpeak();
-//void onReceive();
+void onReceive();
+void normalize(int16_t*, size_t);
+void setFlag();
 //TaskFunction_t speak;
 
 void i2s_install() {
@@ -130,25 +136,33 @@ void i2s_setpin() {
 
 int16_t *new_opus;
 uint8_t *compressed;
-//uint8_t *dataIn;
-//int16_t *uncompressed;
+uint8_t *dataIn;
+int16_t *uncompressed;
+
 
 SemaphoreHandle_t speakSemaphore;
-//SemaphoreHandle_t receiveSemaphore;
+SemaphoreHandle_t receiveSemaphore;
 
+volatile bool receivedFlag = false;
+uint16_t last_received = millis();
 //BluetoothA2DPSink a2dp_sink;
  
 void setup() {
  
   /* Set up Serial Monitor */
   Serial.begin(115200);
-  delay(100);
+  //while(!Serial);
+  delay(3000);
 
+  Serial.println("Serial ready");
   /* Set the speak button */
   pinMode(SPEAK_BUTTON, INPUT_PULLUP);
 
+
   i2s_install();
+  Serial.println("I2S Install Complete");
   i2s_setpin();
+  Serial.println("I2S Pins Set");
 
   pinMode(I2S_CHANNEL_PIN, OUTPUT);
 
@@ -157,8 +171,12 @@ void setup() {
 
   i2s_start(I2S_PORT_IN);
   i2s_start(I2S_PORT_OUT);
-/*
-  radio.begin(LoRa_FREQUENCY, 
+  Serial.println("I2S Start");
+
+  lora_module = new Module(LoRa_NSS, LoRa_DIO1, LoRa_NRST, LoRa_BUSY);
+  radio = new SX1262(lora_module);
+
+  int status = radio->begin(LoRa_FREQUENCY, 
               LoRa_BANDWIDTH, 
               LoRa_SPREADFACTOR, 
               LoRa_CODING_RATE_DENOM, 
@@ -167,14 +185,28 @@ void setup() {
               LoRa_PREAMBLE_LEN, 
               LoRa_TCXO_VOLTAGE, 
               LoRa_USE_REGULATOR_LDO);
-*/
-  delay(100);
+
+
+              delay(100);
+
+              Serial.println("begin");
+              Serial.println(status);
+
+              //radio.setDio1Action(setFlag);
+              status = radio->startReceive();
+              radio->setPacketReceivedAction(setFlag);
+              Serial.println("Status");
+              Serial.println(status);
+              
+
+  //delay(100);
 
   /* Create Encoder */
   int encoder_error;
   opus_encoder_ = opus_encoder_create(AUDIO_SAMPLE_RATE, 1, OPUS_APPLICATION_VOIP, &encoder_error);
   if (encoder_error != OPUS_OK) {
     Serial.println("ENCODE ERROR");
+    Serial.println(encoder_error);
     return;
   }
   encoder_error = opus_encoder_init(opus_encoder_, AUDIO_SAMPLE_RATE, 1, OPUS_APPLICATION_VOIP);
@@ -182,6 +214,7 @@ void setup() {
     Serial.println("ENCODE INIT ERROR");
     return;
   }
+  Serial.println("Encoder Initialized");
   opus_encoder_ctl(opus_encoder_, OPUS_SET_BITRATE(AUDIO_OPUS_BITRATE));
   opus_encoder_ctl(opus_encoder_, OPUS_SET_COMPLEXITY(AUDIO_OPUS_COMPLEXITY));
   opus_encoder_ctl(opus_encoder_, OPUS_SET_SIGNAL(OPUS_SIGNAL_VOICE));
@@ -193,6 +226,7 @@ void setup() {
   opus_decoder_ = opus_decoder_create(AUDIO_SAMPLE_RATE, 1, &decoder_error);
   if (decoder_error != OPUS_OK) {
     Serial.println("DECODE ERROR");
+    Serial.println(decoder_error);
     return;
   } 
 
@@ -202,17 +236,24 @@ void setup() {
   compressed = (uint8_t*) malloc(1275 * sizeof(uint8_t));
   assert(compressed != NULL);
 
-  //dataIn = (uint8_t*) malloc(1275 * sizeof(uint8_t));
-  //assert(dataIn != NULL);
+  dataIn = (uint8_t*) malloc(1275 * sizeof(uint8_t));
+  assert(dataIn != NULL);
 
- // uncompressed = (int16_t*) malloc(OPUS_FRAME_SIZE * sizeof(int16_t));
-  //assert(uncompressed != NULL);
+  uncompressed = (int16_t*) malloc(OPUS_FRAME_SIZE * sizeof(int16_t));
+  assert(uncompressed != NULL);
+/*
+  uncompressed2 = (int16_t*) malloc(OPUS_FRAME_SIZE * sizeof(int16_t));
+  assert(uncompressed2 != NULL);
+
+  uncompressed3 = (int16_t*) malloc(OPUS_FRAME_SIZE * sizeof(int16_t));
+  assert(uncompressed3 != NULL);
+  */
 
   speakSemaphore = xSemaphoreCreateBinary();
   xSemaphoreGive(speakSemaphore);
 
-  //receiveSemaphore = xSemaphoreCreateBinary();
-  //xSemaphoreGive(receiveSemaphore);
+  receiveSemaphore = xSemaphoreCreateBinary();
+  xSemaphoreGive(receiveSemaphore);
 
   Serial.print("SETUP COMPLETE\n");
   delay(100);
@@ -223,13 +264,16 @@ void loop() {
   if (digitalRead(SPEAK_BUTTON) == LOW) {
 
     if (xSemaphoreTake(speakSemaphore, 5) == pdTRUE) {
-      Serial.println("1");
+      //Serial.println("1");
 
       xTaskCreatePinnedToCore(
         [](void *){
+          radio->standby();
           while (digitalRead(SPEAK_BUTTON) == LOW) {
             onSpeak();
+            //yield();
           }
+          radio->startReceive();
           xSemaphoreGive(speakSemaphore);
           vTaskDelete(NULL);
         },
@@ -242,17 +286,32 @@ void loop() {
       );
     } 
   }
-/*
-  if (radio.available() > 0) {
 
+  //Will not work, must be interrupt driven instead (see last chat)
+  //check if packet is available (doesn't actually read yet)
+  if (receivedFlag && digitalRead(SPEAK_BUTTON) != LOW) {
+    //Serial.println("Success");
     if (xSemaphoreTake(receiveSemaphore, 0) == pdTRUE) {
 
+
       xTaskCreatePinnedToCore(
-        [](void *){
-          while (radio.readData(dataIn, radio.getPacketLength()) == RADIOLIB_ERR_NONE) {
+        [](void *) {
+          int count = 0;
+          while (receivedFlag) {
             onReceive();
+            receivedFlag = false;
+            radio->startReceive();
+            if (count > 2) {
+              yield();
+              count = 0;
+            }
+            else {
+              count++;
+            }
+
           }
           xSemaphoreGive(receiveSemaphore);
+          vTaskDelete(NULL);
         },
         "receiveTask",     // name
         8192 * 8,            // stack size in bytes
@@ -263,24 +322,81 @@ void loop() {
       );
     }
   }
-    */
   
 }
-/*
+
 void onReceive() {
+  //Serial.println("Receiving");
 
-  int test = opus_decode(opus_decoder_, dataIn, test, uncompressed, OPUS_FRAME_SIZE, 0);
+  int status = radio->readData(dataIn, radio->getPacketLength());
+  if (status == RADIOLIB_ERR_NONE) {
+    //last_received = millis();
+    /*
+    int16_t *uncompressed;
+    if (curr_buffer == 1) {
+      uncompressed = uncompressed1;
+      curr_buffer = 2;
+      buffers_full++;
+     // buf1 = true;
+    }
+    else if (curr_buffer == 2) {
+      uncompressed = uncompressed2;
+      curr_buffer = 3;
+      buffers_full++;
+      //buf2 = true;
+    }
+    else {
+      uncompressed = uncompressed3;
+      curr_buffer = 1;
+      buffers_full++;
+      //buf3 = true;
+    }
+      */
 
-  size_t num_out;
-  i2s_write(I2S_PORT_OUT, new_opus, OPUS_FRAME_SIZE * sizeof(int16_t), &num_out, 0);
+    int decoded_size = opus_decode(opus_decoder_, dataIn, radio->getPacketLength(), uncompressed, OPUS_FRAME_SIZE, 0);
 
-  memset(dataIn, 0, 1275 * sizeof(uint8_t));
-  memset(uncompressed, 0, OPUS_FRAME_SIZE * sizeof(int16_t));
+    //normalize(uncompressed, decoded_size);
+
+    size_t num_out;
+    /*
+    if (buffers_full == 3) {
+
+      if (next_out == 1) {
+        uncompressed = uncompressed1;
+        next_out = 2;
+        buffers_full--;
+       // buf1 = false;
+      }
+      else if (next_out == 2) {
+        uncompressed = uncompressed2;
+        next_out = 3;
+        buffers_full--;
+       // buf2 = false;
+      }
+      else {
+        uncompressed = uncompressed3;
+        next_out = 1;
+        buffers_full--;
+        //buf3 = false;
+      }
+        */
+
+    i2s_write(I2S_PORT_OUT, uncompressed, OPUS_FRAME_SIZE * sizeof(int16_t), &num_out, 0);
+    //}
+    
+   // memset(dataIn, 0, 1275 * sizeof(uint8_t));
+    //memset(uncompressed, 0, OPUS_FRAME_SIZE * sizeof(int16_t));
+  }
+  else {
+    Serial.println("Error = " + status);
+  }
 
 }
 
 
 void onSpeak() {
+
+  //Serial.println("Speaking");
 
   size_t num_in;
 
@@ -289,28 +405,61 @@ void onSpeak() {
 
   memset(compressed, 0, 1275 * sizeof(uint8_t));
   size_t len = opus_encode(opus_encoder_, new_opus, OPUS_FRAME_SIZE, compressed, 1275 * sizeof(uint8_t));
+  //Serial.println("Length");
+  //Serial.println(len);
 
-  radio.transmit(compressed, len);
+  int test = radio->transmit(compressed, len);
+  //Serial.println("Result");
+  //Serial.println(test);
 
 }
-  */
+  /*
 
 void onSpeak() {
 
-    Serial.println("2");
+    //Serial.println("2");
     size_t num_in;
     memset(new_opus, 0, OPUS_FRAME_SIZE * sizeof(int16_t));
 
     esp_err_t err = i2s_read(I2S_PORT_IN, new_opus, OPUS_FRAME_SIZE * sizeof(int16_t), &num_in, portMAX_DELAY);
-    Serial.println("3");
+    //Serial.println("3");
     int test = opus_encode(opus_encoder_, new_opus, OPUS_FRAME_SIZE, compressed, 1275 * sizeof(uint8_t));
-    Serial.println("4");
+    //Serial.println("4");
     
     test = opus_decode(opus_decoder_, compressed, test, new_opus, 
                 OPUS_FRAME_SIZE, 0);
-                Serial.println("5");
+                //Serial.println("5");
 
     size_t num_out;
     i2s_write(I2S_PORT_OUT, new_opus, OPUS_FRAME_SIZE * sizeof(int16_t), &num_out, 0);
-    Serial.println("6");
+    //Serial.println("6");
+}
+*/
+
+void setFlag(void) {
+  receivedFlag = true;
+}
+
+void normalize(int16_t *buf, size_t length) {
+
+  int32_t max_val = 0;
+
+  for (int i = 0; i < length; i++) {
+    if (abs(buf[i]) > max_val) {
+      max_val = buf[i];
+    }
+  }
+
+  int32_t target_peak = 32767;
+  float gain_factor = (max_val == 0) ? 1.0f : (float)target_peak / max_val;
+  
+  for (int i = 0; i < length; ++i) {
+    int32_t amplified = buf[i] * GAIN_FACTOR;
+    if (amplified > 32767) amplified = 32767;
+    if (amplified < -32768) amplified = -32768;
+    buf[i] = (int16_t)amplified;
+ }
+
+
+  
 }
